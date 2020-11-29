471e9057a1362e93c824cf47c1f2383dcc8bd3cc
Thom Wright <dev@thomwright.co.uk>
2020-11-29 21:28:52 +0000

    Print some colourful TCP packets
    Will hopefully be useful for debugging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use etherparse;
 use etherparse::{
+    InternetSlice,
     InternetSlice::{Ipv4, Ipv6},
+    IpTrafficClass,
     TransportSlice::{Tcp, Udp},
 };
 use std::fmt;
@@ -12,6 +14,7 @@ use tun_tap;
 use ip_proto_id::ip_protocol;
 
 mod ip_proto_id;
+mod print;
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct ConnectionId {
@@ -35,6 +38,9 @@ fn main() -> io::Result<()> {
     // We don't care about the 4 byte header
     let iface = tun_tap::Iface::without_packet_info("tun_tit%d", tun_tap::Mode::Tun)?;
 
+    print::tcp_key();
+    println!();
+
     loop {
         // Ethernet MTU = 1500
         let mut buf = [0u8; 1500];
@@ -45,37 +51,26 @@ fn main() -> io::Result<()> {
         match etherparse::SlicedPacket::from_ip(packet) {
             Err(error) => eprintln!("Error: {}", error),
             Ok(sliced_packet) => {
-                if let Some(ip) = sliced_packet.ip {
-                    let (src, dst, protocol) = match ip {
-                        Ipv4(header) => (
-                            IpAddr::V4(header.source_addr()),
-                            IpAddr::V4(header.destination_addr()),
-                            header.protocol(),
-                        ),
-                        Ipv6(header, _extension) => (
-                            IpAddr::V6(header.source_addr()),
-                            IpAddr::V6(header.destination_addr()),
-                            header.next_header(),
-                        ),
-                    };
+                if let Some(ip) = &sliced_packet.ip {
+                    let (src, dst) = get_ip_addresses(&ip);
+
                     // TODO: consider responding to pings (protocol=1 ICMP)
-                    println!(
-                        "{} -> {}\nprotocol: {:?} - payload: {} bytes ",
-                        src,
-                        dst,
-                        ip_protocol(protocol).expect("Unknown IP protocol number"),
-                        sliced_packet.payload.len(),
-                    );
-                    if let Some(transport) = sliced_packet.transport {
+
+                    print::ip_packet_overview(&sliced_packet, &ip);
+
+                    if let Some(transport) = &sliced_packet.transport {
                         match transport {
                             Tcp(header) => {
+
                                 let conn_id = ConnectionId {
                                     src_addr: src,
                                     src_port: header.source_port(),
                                     dst_addr: dst,
                                     dst_port: header.destination_port(),
                                 };
-                                println!("TCP {}", conn_id);
+
+                                println!("{}", conn_id);
+                                print::tcp_header(&header);
                             }
                             Udp(_header) => {}
                         }
@@ -85,4 +80,27 @@ fn main() -> io::Result<()> {
             }
         }
     }
+}
+
+pub fn get_ip_addresses(ip: &InternetSlice) -> (IpAddr, IpAddr) {
+    match ip {
+        Ipv4(header) => (
+            IpAddr::V4(header.source_addr()),
+            IpAddr::V4(header.destination_addr()),
+            // header.protocol(),
+        ),
+        Ipv6(header, _extension) => (
+            IpAddr::V6(header.source_addr()),
+            IpAddr::V6(header.destination_addr()),
+            // header.next_header(),
+        ),
+    }
+}
+
+pub fn get_next_protocol(ip: &InternetSlice) -> IpTrafficClass {
+    ip_protocol(match ip {
+        Ipv4(header) => header.protocol(),
+        Ipv6(header, _extension) => header.next_header(),
+    })
+    .expect("Unknown IP protocol number")
 }
---
+++ src/print.rs
@@ -0,0 +1,204 @@
+use ansi_term::Colour;
+use etherparse;
+use etherparse::{InternetSlice, SlicedPacket, TcpHeaderSlice};
+
+// TODO: Would be nice to define these as `Style`s, not colours, and maybe to use unique styles
+const SRC_PORT: Colour = Colour::Red;
+const DST_PORT: Colour = Colour::Cyan;
+const SEQ_NUM: Colour = Colour::Blue;
+const ACK_NUM: Colour = Colour::Purple;
+const DATA_OFFSET: Colour = Colour::Green;
+const RESERVED: Colour = Colour::White;
+/// RFC 3540
+const NS: Colour = Colour::Yellow;
+/// RFC 3168
+const CWR: Colour = Colour::Blue;
+/// RFC 3168
+const ECE: Colour = Colour::Cyan;
+const URG: Colour = Colour::Red;
+const ACK: Colour = Colour::Purple;
+const PSH: Colour = Colour::Green;
+const RST: Colour = Colour::Yellow;
+const SYN: Colour = Colour::Blue;
+const FIN: Colour = Colour::Red;
+const WINDOW: Colour = Colour::Green;
+const CHECKSUM: Colour = Colour::Cyan;
+const URGENT: Colour = Colour::Red;
+const OPTIONS: Colour = Colour::Purple;
+
+pub fn tcp_key() {
+  for s in &[
+    SRC_PORT.paint("Source port"),
+    DST_PORT.paint("Destination port"),
+    SEQ_NUM.paint("Sequence number"),
+    ACK_NUM.paint("Acknowledgement number"),
+    DATA_OFFSET.paint("Data offset"),
+    RESERVED.paint("Reserved"),
+    NS.paint("NS: Nonce sum"),
+    CWR.paint("CWR: Congestion window reduced"),
+    ECE.paint("ECE: Explicit congestion notification echo"),
+    URG.paint("URG: Urgent Pointer field significant"),
+    ACK.paint("ACK: Acknowledgment field significant"),
+    PSH.paint("PSH: Push function"),
+    RST.paint("RST: Reset the connection"),
+    SYN.paint("SYN: Synchronize sequence numbers"),
+    FIN.paint("FIN: No more data from sender"),
+    WINDOW.paint("Window"),
+    CHECKSUM.paint("Checksum"),
+    URGENT.paint("Urgent pointer"),
+    OPTIONS.paint("Options"),
+  ] {
+    println!("{}", s);
+  }
+}
+
+// TODO: print like this from the spec!
+// 0                   1                   2                   3
+// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |          Source Port          |       Destination Port        |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                        Sequence Number                        |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                    Acknowledgment Number                      |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |  Data |           |U|A|P|R|S|F|                               |
+// | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
+// |       |           |G|K|H|T|N|N|                               |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |           Checksum            |         Urgent Pointer        |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                    Options                    |    Padding    |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                             data                              |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+/// TODO: we should look at the actual data in the header
+const RESERVED_VAL: u8 = 0;
+
+pub fn tcp_header(header: &TcpHeaderSlice) {
+  tcp_header_hex_line(&header);
+  tcp_header_binary(&header);
+}
+
+pub fn tcp_header_binary(header: &TcpHeaderSlice) {
+  let bin_source_port = format!("{:016b}", header.source_port());
+  let bin_destination_port = format!("{:016b}", header.destination_port());
+  let bin_sequence_number = format!("{:032b}", header.sequence_number());
+  let bin_acknowledgment_number = format!("{:032b}", header.acknowledgment_number());
+  let bin_data_offset = format!("{:04b}", header.data_offset());
+  let bin_reserved = format!("{:03b}", RESERVED_VAL);
+  let bin_ns = format!("{:01b}", if header.ns() { 1 } else { 0 });
+  let bin_cwr = format!("{:01b}", if header.cwr() { 1 } else { 0 });
+  let bin_ece = format!("{:01b}", if header.ece() { 1 } else { 0 });
+  let bin_urg = format!("{:01b}", if header.urg() { 1 } else { 0 });
+  let bin_ack = format!("{:01b}", if header.ack() { 1 } else { 0 });
+  let bin_psh = format!("{:01b}", if header.psh() { 1 } else { 0 });
+  let bin_rst = format!("{:01b}", if header.rst() { 1 } else { 0 });
+  let bin_syn = format!("{:01b}", if header.syn() { 1 } else { 0 });
+  let bin_fin = format!("{:01b}", if header.fin() { 1 } else { 0 });
+  let bin_window_size = format!("{:016b}", header.window_size());
+  let bin_checksum = format!("{:016b}", header.checksum());
+  let bin_urgent_pointer = format!("{:016b}", header.urgent_pointer());
+
+  println!(
+    "{}{}\n{}\n{}\n{}{}{}{}{}{}{}{}{}{}{}{}\n{}{}",
+    SRC_PORT.paint(bin_source_port),
+    DST_PORT.paint(bin_destination_port),
+    SEQ_NUM.paint(bin_sequence_number),
+    ACK_NUM.paint(bin_acknowledgment_number),
+    DATA_OFFSET.paint(bin_data_offset),
+    RESERVED.paint(bin_reserved),
+    NS.paint(bin_ns),
+    CWR.paint(bin_cwr),
+    ECE.paint(bin_ece),
+    URG.paint(bin_urg),
+    ACK.paint(bin_ack),
+    PSH.paint(bin_psh),
+    RST.paint(bin_rst),
+    SYN.paint(bin_syn),
+    FIN.paint(bin_fin),
+    WINDOW.paint(bin_window_size),
+    CHECKSUM.paint(bin_checksum),
+    URGENT.paint(bin_urgent_pointer),
+  );
+
+  for four_bytes in header.options().chunks(4) {
+    for byte in four_bytes {
+      print!("{}", OPTIONS.paint(format!("{:08b}", byte)));
+    }
+    println!();
+  }
+}
+
+pub fn tcp_header_hex_line(header: &TcpHeaderSlice) {
+  let hex_source_port = format!("{:04x}", header.source_port());
+  let hex_destination_port = format!("{:04x}", header.destination_port());
+  let hex_sequence_number = format!("{:08x}", header.sequence_number());
+  let hex_acknowledgment_number = format!("{:08x}", header.acknowledgment_number());
+  let hex_data_offset = format!("{:01x}", header.data_offset());
+  let hex_reserved = format!("{:01x}", RESERVED_VAL);
+  let hex_ns = format!("{:01x}", if header.ns() { 1 } else { 0 });
+  let hex_cwr = format!("{:01x}", if header.cwr() { 1 } else { 0 });
+  let hex_ece = format!("{:01x}", if header.ece() { 1 } else { 0 });
+  let hex_urg = format!("{:01x}", if header.urg() { 1 } else { 0 });
+  let hex_ack = format!("{:01x}", if header.ack() { 1 } else { 0 });
+  let hex_psh = format!("{:01x}", if header.psh() { 1 } else { 0 });
+  let hex_rst = format!("{:01x}", if header.rst() { 1 } else { 0 });
+  let hex_syn = format!("{:01x}", if header.syn() { 1 } else { 0 });
+  let hex_fin = format!("{:01x}", if header.fin() { 1 } else { 0 });
+  let hex_window_size = format!("{:04x}", header.window_size());
+  let hex_checksum = format!("{:04x}", header.checksum());
+  let hex_urgent_pointer = format!("{:04x}", header.urgent_pointer());
+
+  for s in &[
+    SRC_PORT.paint(&hex_source_port),
+    DST_PORT.paint(&hex_destination_port),
+    SEQ_NUM.paint(&hex_sequence_number),
+    ACK_NUM.paint(&hex_acknowledgment_number),
+    DATA_OFFSET.paint(&hex_data_offset),
+    RESERVED.paint(&hex_reserved),
+  ] {
+    print!("{} ", s);
+  }
+
+  for f in &[
+    NS.paint(&hex_ns),
+    CWR.paint(&hex_cwr),
+    ECE.paint(&hex_ece),
+    URG.paint(&hex_urg),
+    ACK.paint(&hex_ack),
+    PSH.paint(&hex_psh),
+    RST.paint(&hex_rst),
+    SYN.paint(&hex_syn),
+    FIN.paint(&hex_fin),
+  ] {
+    print!("{}", f);
+  }
+  print!(" ");
+
+  for s in &[
+    WINDOW.paint(&hex_window_size),
+    CHECKSUM.paint(&hex_checksum),
+    URGENT.paint(&hex_urgent_pointer),
+  ] {
+    print!("{} ", s);
+  }
+
+  for o in header.options() {
+    print!("{}", OPTIONS.paint(format!("{:x}", o)));
+  }
+  println!();
+}
+
+pub fn ip_packet_overview(sliced_packet: &SlicedPacket, ip: &InternetSlice) {
+  let (src, dst) = crate::get_ip_addresses(ip);
+  let inner_protocol = crate::get_next_protocol(ip);
+  println!(
+    "{} -> {}\nprotocol: {:?} - payload: {} bytes ",
+    src,
+    dst,
+    inner_protocol,
+    sliced_packet.payload.len(),
+  );
+}
