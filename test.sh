04c771dae4e891492df988aeec3cf9e390a3ce4e
Thom Wright <dev@thomwright.co.uk>
2020-12-11 17:22:11 +0000

    Add netstat to test script
    So we can see the state of the client socket.
    
    netcat doesn't seem to verify that the socket closed properly,
    so it can exit with a 0 status code even though we don't yet
    respond to FINs, and the client socket gets left in FIN_WAIT1.

--- test.sh
+++ test.sh
@@ -5,4 +5,11 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
-nc 10.0.0.10 80
+ip_addr=10.0.0.10
+port=4433
+
+nc -vz $ip_addr $port
+
+printf "\n"
+
+netstat -tanpo 2>/dev/null | grep "$ip_addr:$port"
